036f3d93a9b0a29ec9bbfcd56ef628462dbe8821
Angel Guerrero <angelemilioguerrero@gmail.com>
2021-01-20 22:29:12 -0400

    Update PHP Folder, First Test PHP and HTML

---
+++ PHP/MasterPHP/php03 variable/index.php
@@ -0,0 +1,10 @@
+<?php
+//Una variable es contenedor de informacion para salvar datos en la misma
+    $primera_variable='Hola desde variable';
+    $numero=44;
+    $verdadero=true;
+
+    echo '<h2>'.$primera_variable.'</h2>';
+    echo $numero.'<=>'. $verdadero
+
+?>
