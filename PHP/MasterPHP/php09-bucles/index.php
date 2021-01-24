92e10160552f5cd2561db8f941e22827d69406ea
Angel Guerrero <angelemilioguerrero@gmail.com>
2021-01-23 21:18:53 -0400

    Update bucles

--- PHP/MasterPHP/php09-bucles/index.php
+++ PHP/MasterPHP/php09-bucles/index.php
@@ -15,5 +15,41 @@ while ($numero<=100){
     $numero++;
 }
 
+echo '<hr/>';
+//isset, si existe un valor en el GET
+if (isset($_GET['numero'])){
+    //Casting
+    $numero=(int)$_GET['numero'];
+
+
+}else {
+$numero=1;
+}
+
+echo "<h1>Tabla de Multiplicar del numero $numero</h1>";
+$contador=0;
+while ($contador<=10){
+echo "$numero x $contador = " .($numero* $contador). "<br/>";
+$contador++;
+
+}
+
+var_dump($numero);
+
+
+echo "<hr/>";
+
+/*DO WHILE
+do {}while();
+Hacer Mietras
+*/ 
+
+$edad=15;
+$contador=1;
+
+do {
+    echo "Tienes Acceso al local privado $contador".'<br/>';
+    $contador++;
+}while($edad>=18 AND $contador<=10);
 
 ?>
