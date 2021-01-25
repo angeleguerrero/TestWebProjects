1bda0f0fc199794e1f927e903c10c65a639a0976
Angel Guerrero <angelemilioguerrero@gmail.com>
2021-01-24 22:42:30 -0400

    Update Ejercicios mod 11

---
+++ PHP/MasterPHP/php11-Exercises/excer-01.php
@@ -0,0 +1,14 @@
+<?php
+    
+    /*Crear variables pais y continente y mostrar valor por pantalla, colocar en comentario tipo de dato*/
+
+
+    $pais='Rep Dominicana'; //Tipo de dato String
+    $continente='Americano'; //Tipo de dato String
+
+    echo 'El pais es: '. $pais. ' y el continen es continente es: ' . $continente;
+
+    
+    
+
+?>
